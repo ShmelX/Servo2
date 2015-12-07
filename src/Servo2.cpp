3a1e34aab92e420aefee108cd43e211c96575a88
takumusu <me@takumus.com>
2015-12-07 11:04:50 +0900

    Update Servo2.cpp

--- src/Servo2.cpp
+++ src/Servo2.cpp
@@ -13,5 +13,10 @@ void Servo2::write(float rotation)
 {
     if(rotation > _maxRotation) rotation = _maxRotation;
     if(rotation < 0) rotation = 0;
+    _rotation = rotation;
     _servo.writeMicroseconds(rotation / _maxRotation * _defPulse + _minPulse);
+}
+float Servo2::read()
+{
+    return _rotation;
 }
