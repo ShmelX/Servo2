cb967c8528665d9a94c4a43522986f9040d86d4f
takumusu <me@takumus.com>
2015-12-07 11:03:43 +0900

    Update Servo2.h

--- src/Servo2.h
+++ src/Servo2.h
@@ -9,11 +9,12 @@ class Servo2
 public:
     void attach(int out, int minPulse, int maxPulse, int maxRotation);
     void write(float rotation);
-    
+    float read();
 private:
     int _minPulse, _maxPulse;
     float _maxRotation;
     float _defPulse, _defRotation;
+    float _rotation;
     Servo _servo;
 };
  
